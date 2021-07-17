ef543807b245a05b1dffad53b1a666db62abc02a
John112222 <87584863+John112222@users.noreply.github.com>
2021-07-17 15:41:46 -0700

    Created Main Menu

---
+++ Assets/Scripts/MenuUtilityScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUtilityScript : MonoBehaviour
+{
+   public static void QuitGame(){
+       #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+       Application.Quit();
+   }
+}
