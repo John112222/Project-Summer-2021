aa7e8fdbd781ff5262597fce201d4fa7bb2413c9
John112222 <87584863+John112222@users.noreply.github.com>
2021-08-07 14:10:02 -0700

    Created Attempted Fix For FPS Controller

--- Assets/Scripts/PlayerSpawnerManager.cs
+++ Assets/Scripts/PlayerSpawnerManager.cs
@@ -6,9 +6,14 @@ using Photon.Pun;
 public class PlayerSpawnerManager : MonoBehaviourPunCallbacks
 {
     public GameObject playerprefab;
+    public GameObject Networkcopyprefab;
     // Start is called before the first frame update
     void Start()
     {
+        if(!photonView.IsMine){
+            GameObject Model=PhotonNetwork.Instantiate(this.Networkcopyprefab.name,Vector3.zero,Quaternion.identity,0);
+            return;
+        }
         if(playerprefab==null){
             Debug.LogError("Missing Player Prefab");
             return;
