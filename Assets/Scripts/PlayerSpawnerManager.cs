72b6537f630521b3d3f7da985fee9342f6baf6c9
John112222 <87584863+John112222@users.noreply.github.com>
2021-08-07 14:42:07 -0700

    Updated FPS Controller

--- Assets/Scripts/PlayerSpawnerManager.cs
+++ Assets/Scripts/PlayerSpawnerManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class PlayerSpawnerManager : MonoBehaviourPunCallbacks
 {
@@ -10,6 +11,17 @@ public class PlayerSpawnerManager : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
+        if(PhotonNetwork.IsMasterClient){
+            foreach(Player P in PhotonNetwork.PlayerList){
+                GameObject player=PhotonNetwork.Instantiate(this.playerprefab.name,Vector3.zero,Quaternion.identity,0);
+                PhotonView pv =player.GetComponent<PhotonView>();
+                pv.TransferOwnership(P);
+                
+            }
+        }
+            return;
+
+
         if(!photonView.IsMine){
             GameObject Model=PhotonNetwork.Instantiate(this.Networkcopyprefab.name,Vector3.zero,Quaternion.identity,0);
             return;
