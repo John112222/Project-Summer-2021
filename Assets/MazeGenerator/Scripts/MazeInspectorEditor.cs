0cd984a5b3085c4ee0e67dedaf757c454d5f7ed1
John112222 <87584863+John112222@users.noreply.github.com>
2021-11-22 17:59:55 -0800

    window added

---
+++ Assets/MazeGenerator/Scripts/MazeInspectorEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MazeSpawner))]
+public class MazeInspectorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MazeSpawner spawner = (MazeSpawner)target;
+        if (GUILayout.Button("Create Maze"))
+        {
+            spawner.CreateMaze();
+        }
+        if (GUILayout.Button("Delete All Maze"))
+        {
+            spawner.ResetMaze();
+        }
+    }
+}
---
+++ Assets/Scripts/Temp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Initialize(int ActorNumber,string nickname)
+
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void SetPlayerReady(bool isready)
+    
+    {
+        
+    }
+}
